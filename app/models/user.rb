841d7f7cfaab3617b1fd275f4f68389c275b4fad
Anemone0203 <lady.murasaki@gmail.com>
2021-01-17 11:45:09 +0900

    パスワードのバリデーション追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-   validatables :nickname, presence :true
+   validates :nickname, presence: true
   #  validatables :email, presence :true
-   validatables :first_name, presence :true
-   validatables :last_name, presence :true
-   validatables :first_name_kana, presence :true
-   validatables :last_name_kana, presence :true
-   validatables :birth, presence :true
+   validates :first_name, presence: true
+   validates :last_name, presence: true
+   validates :first_name_kana, presence: true
+   validates :last_name_kana, presence: true
+   validates :birth, presence: true
+   VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/
+   validates :password,format: { with: VALID_PASSWORD_REGEX}
+            # message: "は半角英数字 混合"
 end
 
-has_many :items
-has_many :buys
+# has_many :items
+# has_many :buys
