8d96d25ee2ed5043337a533ee38f80f39e7a0ddb
Anemone0203 <lady.murasaki@gmail.com>
2021-01-17 09:35:55 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+   validatables :nickname, presence :true
+  #  validatables :email, presence :true
+   validatables :first_name, presence :true
+   validatables :last_name, presence :true
+   validatables :first_name_kana, presence :true
+   validatables :last_name_kana, presence :true
+   validatables :birth, presence :true
 end
+
+has_many :items
+has_many :buys
