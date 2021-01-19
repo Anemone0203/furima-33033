033bdec770be4620a2b1d37f7a95a1f4dd71e446
Anemone0203 <lady.murasaki@gmail.com>
2021-01-19 12:56:36 +0900

    ActiveHash導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,23 @@
 class ItemsController < ApplicationController
 
-def index
-  @items = Item.all
-end
+  def index
+    @items = Item.all
+  end
 
-def new
-  @Item = Item.new
-end
+  def new
+    @Item = Item.new
+  end
 
-def create
-  Item.create(tweet_params)
-end
+  def create
+    Item.create(tweet_params)
+   if current_user.create(tweet_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  # rescue => exception
+  end
+
 
 
  private
