444305de29db979716ed0ec222c4a2352ce7f270
Anemone0203 <lady.murasaki@gmail.com>
2021-01-24 20:16:59 +0900

    編集機能、エラー表示の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,16 +22,26 @@ class ItemsController < ApplicationController
   end
 
 
+
   def edit
     @item = Item.find(params[:id])
   end
 
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
+    @item = Item.find(params[:id])
+    # @item.update(item_params)
+     if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+  end
 
   private
 
